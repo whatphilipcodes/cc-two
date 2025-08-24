2ad9fb61057a8dc276220d11585b5410daf8705d
whatphilipcodes <gerdes.philip@gmail.com>
2025-08-24 14:03:24 +0200

    fix: atm remaining artifacts

--- AberrProj/AberrProj/AberrViewModel.swift
+++ AberrProj/AberrProj/AberrViewModel.swift
@@ -5,22 +5,22 @@
 //  Created by Philip Gerdes on 22.08.25.
 //
 
-import ATMPackage
+import AberrPackage
 import Foundation
 
 class AberrViewModel: ObservableObject {
     @Published var balance: Int32
-    private var atm: ATMWrapper
+    private var aberr: AberrWrapper
     @Published var libRawVersion: String = ""
 
-    init(atm: ATMWrapper = ATMWrapper(initialBalance: 1000)) {
-        self.atm = atm
-        self.balance = atm.getBalance()
-        self.libRawVersion = atm.libRawVersionInfo()
+    init(aberr: AberrWrapper = AberrWrapper(initialBalance: 1000)) {
+        self.aberr = aberr
+        self.balance = aberr.getBalance()
+        self.libRawVersion = aberr.libRawVersionInfo()
     }
 
     func withdraw(amount: String) {
-        guard let value = Int32(amount), atm.withdraw(amount: value) else { return }
-        balance = atm.getBalance()
+        guard let value = Int32(amount), aberr.withdraw(amount: value) else { return }
+        balance = aberr.getBalance()
     }
 }
--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Bare-bones RAW photo editing software for `iOS`.
 | `2025-08-18` | wasting time with vcpkg                                                                                | 6     |
 | `2025-08-22` | refactoring based on this [guide](https://arturgruchala.com/swift-and-c-interoperability-in-practice/) | 8     |
 | `2025-08-23` | attempting to get `LibRaw` working                                                                     | 8     |
-| `2025-08-24` | finally getting `LibRaw` to compile and build                                                          | tbd   |
+| `2025-08-24` | finally getting `LibRaw` to compile and build & rough cpp package architecture rework                  | tbd   |
 
 
 ### Acknowledgements
